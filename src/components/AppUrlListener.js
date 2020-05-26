78db18e8acca8f6cc0ca58e23dd3045542460e5d
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2020-05-27 01:56:48 +0530

    deep link initialization

--- src/components/AppUrlListener.js
+++ src/components/AppUrlListener.js
@@ -1,25 +1,9 @@
 import React, { useEffect } from 'react';
-import { Plugins } from '@capacitor/core';
-const { App } = Plugins;
 
 const AppUrlListener = (props) => {
 	useEffect(() => {
-		console.log(window.location);
-		// const slug = window.location.href.url.split('.app').pop();
-		// console.log(slug);
-		App.addListener('appUrlOpen', (any) => {
-			console.log('here');
-			// Example url: https://beerswift.app/tabs/tab2
-			// slug = /tabs/tab2
-
-			console.log('here1');
-			window.open('https://www.flipkart.com/');
-			// if (slug) {
-			// 	props.history.push(slug);
-			// }
-			// If no match, do nothing - let regular routing
-			// logic take over
-		});
+		const slug = window.location.pathname;
+		console.log(slug);
 	}, []);
 
 	return null;
