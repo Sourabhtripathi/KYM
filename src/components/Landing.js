3931bfd401260ac5bbecb9d083c7b62f98d52b62
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2020-05-27 16:41:37 +0530

    reset prev commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { IonContent } from '@ionic/react';
 import { serverUrl } from '../variables';
-import { login } from '../helpers';
 
 const Landing = () => {
-	const onLogin = async () => {
-		const res = await login();
-		console.log(res);
-	};
-
 	return (
 		<IonContent>
 			<h1>Landing</h1>
-			{/* <a href={`${serverUrl}/login`}>Login with Spotify</a> */}
-			<button onClick={onLogin}>Login</button>
+			<a href={`${serverUrl}/login`}>Login with Spotify</a>
 		</IonContent>
 	);
 };
