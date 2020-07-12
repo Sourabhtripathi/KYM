3d02f75e82f282e819c4e7779065993e19665f27
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2020-07-12 23:13:04 +0530

    change server url

--- src/variables.js
+++ src/variables.js
@@ -1,2 +1,2 @@
-export const serverUrl = 'http://localhost:3005';
-// export const serverUrl = 'https://kymserver.herokuapp.com';
+// export const serverUrl = 'http://localhost:3005';
+export const serverUrl = 'https://kymserver.herokuapp.com';
